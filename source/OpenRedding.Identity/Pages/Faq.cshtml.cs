1c1aa70b076919a54816cd0c0f57bb9e395fbfd9
Joey Mckenzie <joey.mckenzie27@gmail.com>
2020-03-14 10:54:16 -0700

    feat(Identity): Add finishing touches to login page UI

---
+++ source/OpenRedding.Identity/Pages/Faq.cshtml.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenRedding.Identity.Pages
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#pragma warning disable SA1649 // File name should match first type name
+    public class FaqModel : PageModel
+#pragma warning restore SA1649 // File name should match first type name
+    {
+        public async Task OnGetAsync()
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(500));
+        }
+    }
+}
