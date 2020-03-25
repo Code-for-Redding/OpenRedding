d08642723dbabc739b87ba1ae6077637dfa73182
Joey Mckenzie <joey.mckenzie27@gmail.com>
2020-03-25 07:10:41 -0700

    feat(Client): Update UI pages

---
+++ source/OpenRedding.Client/wwwroot/js/jsInterop.js
@@ -0,0 +1,33 @@
+﻿// JavaScript interop code to interface with the application
+window.initParticlesJs = () => {
+    let config = {
+        // normal options
+        selector: '.particle-background',
+        maxParticles: 450,
+        // options for breakpoints
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 200,
+                    color: '#48F2E3',
+                    connectParticles: false
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 100,
+                    connectParticles: true
+                }
+            }, {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 0
+                    // disables particles.js
+                }
+            }
+        ]
+    };
+
+    Particles.init(config);
+}
