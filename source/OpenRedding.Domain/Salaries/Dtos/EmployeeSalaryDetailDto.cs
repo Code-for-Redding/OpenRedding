fb2f244d54630d743450cc750a48e56581a3d696
Joey Mckenzie <joey.mckenzie27@gmail.com>
2020-06-09 13:06:03 -0700

    chore(Domain): CodeMain run to fix build errors

--- source/OpenRedding.Domain/Salaries/Dtos/EmployeeSalaryDetailDto.cs
+++ source/OpenRedding.Domain/Salaries/Dtos/EmployeeSalaryDetailDto.cs
@@ -20,52 +20,52 @@ namespace OpenRedding.Domain.Salaries.Dtos
         /// <summary>
         /// Gets or sets the employee job title.
         /// </summary>
-		public string? JobTitle { get; set; }
+        public string? JobTitle { get; set; }
 
         /// <summary>
         /// Gets or sets the employee base pay.
         /// </summary>
-		public decimal BasePay { get; set; }
+        public decimal BasePay { get; set; }
 
         /// <summary>
         /// Gets or sets the employee overtime pay.
         /// </summary>
-		public decimal OvertimePay { get; set; }
+        public decimal OvertimePay { get; set; }
 
         /// <summary>
         /// Gets or sets the employee other pay.
         /// </summary>
-		public decimal OtherPay { get; set; }
+        public decimal OtherPay { get; set; }
 
         /// <summary>
         /// Gets or sets the employee benefit compensation pay.
         /// </summary>
-		public decimal Benefits { get; set; }
+        public decimal Benefits { get; set; }
 
         /// <summary>
         /// Gets or sets the total aggregate employee pay.
         /// </summary>
-		public decimal TotalPay { get; set; }
+        public decimal TotalPay { get; set; }
 
         /// <summary>
         /// Gets or sets the total aggregate employee pay including benefits.
         /// </summary>
-		public decimal TotalPayWithBenefits { get; set; }
+        public decimal TotalPayWithBenefits { get; set; }
 
         /// <summary>
         /// Gets or sets the reporting year.
         /// </summary>
-		public int Year { get; set; }
+        public int Year { get; set; }
 
         /// <summary>
         /// Gets or sets the employee agency.
         /// </summary>
-		public string? Agency { get; set; }
+        public string? Agency { get; set; }
 
         /// <summary>
         /// Gets or sets the employee employment status.
         /// </summary>
-		public string? Status { get; set; }
+        public string? Status { get; set; }
 
         /// <summary>
         /// Gets or sets the employee self link.
