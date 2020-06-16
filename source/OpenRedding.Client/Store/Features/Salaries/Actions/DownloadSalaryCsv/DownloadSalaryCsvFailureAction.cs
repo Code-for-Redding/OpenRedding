41e4050673a6cdc9b967bf963fcf7d09598e7f1a
Joey Mckenzie <joey.mckenzie27@gmail.com>
2020-06-16 12:42:36 -0700

    chore(Client): CodeMaid cleanup

--- source/OpenRedding.Client/Store/Features/Salaries/Actions/DownloadSalaryCsv/DownloadSalaryCsvFailureAction.cs
+++ source/OpenRedding.Client/Store/Features/Salaries/Actions/DownloadSalaryCsv/DownloadSalaryCsvFailureAction.cs
@@ -1,6 +1,6 @@
 ﻿namespace OpenRedding.Client.Store.Features.Salaries.Actions.DownloadSalaryCsv
 {
-	using OpenRedding.Client.Store.Features.Shared.Actions;
+    using OpenRedding.Client.Store.Features.Shared.Actions;
 
     public class DownloadSalaryCsvFailureAction : FailureAction
     {
--- source/OpenRedding.Client/Store/Features/Salaries/Actions/LoadEmployeeSalaryDetail/LoadEmployeeSalaryDetailFailureAction.cs
+++ source/OpenRedding.Client/Store/Features/Salaries/Actions/LoadEmployeeSalaryDetail/LoadEmployeeSalaryDetailFailureAction.cs
@@ -1,6 +1,6 @@
 ﻿namespace OpenRedding.Client.Store.Features.Salaries.Actions.LoadEmployeeSalaryDetail
 {
-	using OpenRedding.Client.Store.Features.Shared.Actions;
+    using OpenRedding.Client.Store.Features.Shared.Actions;
 
     public class LoadEmployeeSalaryDetailFailureAction : FailureAction
     {
